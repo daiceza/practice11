36cd87a658fd41264dfb49a0c1c634d5f7fab518
sase_daiki <sase_daiki@yahoo.co.jp>
2019-10-21 11:40:52 +0000

    PHP/Laravel 09課題

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//09課題3 AAAController.phpをApp\Http\Controllers\XXXに作成したと仮定する
+Route::get('XXX','XXX\AAAController@bbb');
+
+Route::group(['prefix' => 'admin'],function(){
+    Route::get('news/create','Admin\NewsController@add');
+    //09課題4
+    Route::get('profile/create','Admin\ProfileConroller@add');
+    Route::get('profile/edit','Admin\ProfileConroller@edit');
+});
