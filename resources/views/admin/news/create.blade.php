db8db30d70c9f577005381aeec34e5da343781fe
sase_daiki <sase_daiki@yahoo.co.jp>
2019-10-22 01:26:38 +0000

    PHP/Laravel 11課題

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -3,11 +3,22 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content=width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         
         <title>MyNews</title>
     </head>
     <body>
+        @extends('layouts.admin')
+        
+        @section('title','ニュースの新規作成')
+        
+        @section('content')
+            <div class="container">
+                <div class="col-md-8 mx-auto">
+                    <h2>ニュース新規作成</h2>
+                </div>
+            </div>
+        @endsection
         <h1>Myニュース作成画面
     </body>
 </html>
