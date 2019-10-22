e0a643f8d986e2e3d84528aafa9fd35804e94064
sase_daiki <sase_daiki@yahoo.co.jp>
2019-10-22 04:38:36 +0000

    PHP/Laravel 11-3修正

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,24 +1,12 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.profile')
         
-        <title>Myprofile</title>
-    </head>
-    <body>
-        @extends('layouts.admin')
-        
-        @section('title','プロフィールの新規作成')
-        @section('content')
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8 mx-auto">
-                        <h2>Myプロフィール</h1>
-                    </div>
-                </div>
+@section('title','プロフィールの新規作成')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール新規作成</h1>
             </div>
-        @endsection
-    </body>
-</html>
+        </div>
+    </div>
+@endsection
